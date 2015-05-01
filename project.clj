b34a117709a11f216b874e53f81aded92a4d3990
kovasb <kovas.boguta@gmail.com>
2015-05-01 10:54:06 -0400

    update project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
-(defproject gamma "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject gamma "0.1.0"
+  :description "glsl shaders made simple"
+  :url "https://github.com/kovasb/gamma"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [kovasb/fipp "0.5.3-SNAPSHOT"]
                  [org.clojure/clojurescript "0.0-3208"]]
-  :source-paths ["src" "test"]
-
-  )
+  :source-paths ["src" "test"])
 
