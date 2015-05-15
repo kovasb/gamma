a07a7322c4ea08a896742bb34d34d81f06773435
kovasb <kovas.boguta@gmail.com>
2015-05-15 00:55:02 -0400

    fix precision api

--- src/gamma/program.cljs
+++ src/gamma/program.cljs
@@ -12,12 +12,23 @@
             [gamma.api :as g]))
 
 
+
+
+(defn ast [inputs]
+  (apply g/block
+         (mapv
+           (fn [[k v]]
+             (g/set k v))
+           inputs)))
+
+
+
 (defn precision-defaults [p]
+  (println p)
   (let [x (map (fn [[k v]] (str "precision " (name v) " " (name k) ";\n")) p)]
     (if (empty? x)
       ""
       (apply str x))))
-
 
 (defn glsl [shader opts]
   (let [p (:precision opts)]
@@ -28,13 +39,6 @@
          (emit/emit (:ir shader) shader)
          {:width 80})))))
 
-
-(defn ast [inputs]
-  (apply g/block
-         (mapv
-           (fn [[k v]]
-             (g/set k v))
-           inputs)))
 
 
 (defn shader [shader opts]
@@ -71,10 +75,10 @@
             (:inputs fs))))
 
 (defn program
-  ([x] (program x {}))
-  ([x opts]
-   (let [{:keys [vertex-shader fragment-shader]} x
-         vs (shader vertex-shader opts) fs (shader fragment-shader opts)]
+  ([x]
+   (let [{:keys [vertex-shader fragment-shader precision]} x
+         vs (shader vertex-shader precision)
+         fs (shader fragment-shader precision)]
      {:tag             :program
       :vertex-shader   vs
       :fragment-shader fs
