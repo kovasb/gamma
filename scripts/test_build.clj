8c71dd6c519d45316f79ca52c208d0b31fae62d1
Roger Allen <rallen@gmail.com>
2016-08-20 12:27:12 -0700

    Document & enable testing
    While I'm not sure this is the best way to test, it works for me.  Added
    instructions to README.md and running tests shows:
    
    Ran 4 tests containing 100 assertions.
    21 failures, 3 errors.

---
+++ scripts/test_build.clj
@@ -0,0 +1,8 @@
+(require '[cljs.closure :as cljsc]
+         '[cljs.build.api])
+
+(cljsc/build (cljs.build.api/inputs "src" "test")
+             {:output-dir "resources/public/js/out"
+              :output-to "resources/public/js/main.js"
+              :main "gamma.test.runner"
+              :asset-path "http://localhost:9000/js/out"})
